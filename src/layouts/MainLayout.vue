53b17ddbbd67d936c6b7f8adcebebe773a8744a4
Matheus Bloise Vieira <46876171+mathbloise@users.noreply.github.com>
2021-02-21 19:34:52 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-dark text-white" height-hint="98" style="height: 15vh">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div class="row justify-end q-mt-md">
+            <div class="col-md-2">
+              <span class="text-weight-bold">Matheus Bloise</span>
+            </div>
+          </div>
+          <div class="row justify-end">
+            <div class="col-md-2">
+              <span>Software Developer</span>
+            </div>
+          </div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="right" side="right" bordered>
+      <div class="q-pa-lg bg-grey-2" style="height: 100%">
+        <div class="row justify-center">
+          <q-avatar size="160px" class="q-mb-sm">
+            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQH5gks7bRvljQ/profile-displayphoto-shrink_800_800/0/1599520901364?e=1619049600&v=beta&t=_3g84yIQRMJiVY9ZOM0WjHAgJwUA5W0WBjYii61ER7w">
+          </q-avatar>
+        </div>
+        <span class="text-weight-bold">@matheusbloise</span>
+        <div style="margin-top: 5px">
+          <span class="text-weight-regular">Technical skills</span>
+        </div>
+
+        <div class="q-pt-md">
+          <span>
+            Development solutions with several technologies,
+            see below some listed skills that i worked to solve real problems.
+          </span>
+        </div>
+        <div class="row q-my-md justify-center">
+          <div class="col-md-3">
+            <q-img
+              src="~assets/php.svg"
+              style="width: 43px"
+            />
+          </div>
+          <div class="col-md-8 q-mt-md">
+            <q-linear-progress color="blue-9" size="10px" :value=0.9 />
+          </div>
+        </div>
+
+        <div class="row q-my-md justify-center">
+          <div class="col-md-3">
+            <q-img
+              src="~assets/js.svg"
+              style="width: 46px"
+            />
+          </div>
+          <div class="col-md-8 q-mt-md">
+            <q-linear-progress color="blue-9" size="10px" :value=0.8 />
+          </div>
+        </div>
+
+        <div class="row q-my-md justify-center">
+          <div class="col-md-3">
+            <q-img
+              src="~assets/pgsql.svg"
+              style="width: 42px"
+            />
+          </div>
+          <div class="col-md-8 q-mt-md">
+            <q-linear-progress color="blue-9" size="10px" :value=0.8 />
+          </div>
+        </div>
+      </div>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      right: false
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,261 @@
+<template>
+  <q-page class="q-pa-md">
+    <div class="q-px-lg q-pb-md">
+    <q-timeline color="blue-9">
+      <q-timeline-entry heading>
+        <p class="text-h3">
+          Resume
+        </p>
+      </q-timeline-entry>
+
+      <q-timeline-entry
+        title="Trade Technology - Artificial Intelligence Software"
+        subtitle="November, 2020 - At the moment"
+        side="left"
+        color="blue-9"
+      >
+      <div>
+        <ol class="q-pa-sm" style="list-style-type: none;">
+          <li class="q-my-sm">
+            <span class="text-weight-bold">Software Developer - Fullstack</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-light">Agile metrics SCRUM</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-light">Contribution to arquiteture solutions</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-light">Improvement codebase, Code-review and Pair Programing</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-light">Dealing with good practices and clean architecture patterns</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-light">Builded microservices in PHP backend >=7.4, fallowing modules pattern and distributed code</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-bold">Technologies: </span>
+            <span class="text-weight-light">PHP, Laravel, Lumen, NodeJS, Express, MongoDB, Postgress, ReactJS</span>
+          </li>
+        </ol>
+      </div>
+      </q-timeline-entry>
+    </q-timeline>
+
+    <q-timeline>
+      <q-timeline-entry
+        title="Bitzen - Software company"
+        subtitle="October, 2019 - November, 2020 . 1 year and 2 months"
+        side="left"
+        color="yellow-8"
+      >
+        <div>
+        <ol class="q-pa-sm" style="list-style-type: none;">
+          <li class="q-my-sm">
+            <span class="text-weight-bold">Software Developer - Fullstack</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-light">Improvement codebase, solutions architect on new projects</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-light">Dealing with good practices, SOLID, KISS, Module pattern arquiteture</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-light">Builded API REST in PHP backend >=7.4 and Dashboard templates on Vuetify - VueJS</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-bold">Technologies: </span>
+            <span class="text-weight-light">PHP, Laravel, Mysql, VueJS, Angular, IONIC</span>
+          </li>
+        </ol>
+      </div>
+      </q-timeline-entry>
+    </q-timeline>
+
+    <q-timeline>
+      <q-timeline-entry
+        title="Elotech - Governamental Software"
+        subtitle="January, 2019 - June, 2019 . 6 months"
+        side="left"
+        color="yellow-8"
+      >
+        <div>
+          <ol class="q-pa-sm" style="list-style-type: none;">
+            <li class="q-my-sm">
+              <span class="text-weight-bold">Software Developer - Fullstack</span>
+            </li>
+            <li class="q-my-sm">
+              <span class="text-weight-light">Agile metrics - SCRUM, Kanban</span>
+            </li>
+            <li class="q-my-sm">
+              <span class="text-weight-light">Code-review and Pair Programing</span>
+            </li>
+            <li class="q-my-sm">
+              <span class="text-weight-light">Business rules software - Accounting and Tribute</span>
+            </li>
+            <li class="q-my-sm">
+              <span class="text-weight-light">Builded API REST in JAVA Spring Boot </span>
+            </li>
+            <li class="q-my-sm">
+              <span class="text-weight-bold">Technologies: </span>
+              <span class="text-weight-light">Java - spring boot, Gradle, JasperReports, Mysql, ReactJS, Angular</span>
+            </li>
+        </ol>
+      </div>
+      </q-timeline-entry>
+    </q-timeline>
+
+    <q-timeline>
+      <q-timeline-entry
+        title="Rodobens"
+        subtitle="June, 2015 - January, 2017 . 1 year and 8 months"
+        side="left"
+        color="yellow-8"
+      >
+        <div>
+         <ol class="q-pa-sm" style="list-style-type: none;">
+          <li class="q-my-sm">
+            <span class="text-weight-bold">Software Developer Trainee</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-light">Business rules software CRM</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-light">Contribution to development Intranet App</span>
+          </li>
+          <li class="q-my-sm">
+            <span class="text-weight-bold">Technologies: </span>
+            <span class="text-weight-light">HTML, CSS, Javacript, PHP, MySQL, ASP, Java and Software ERP Qualitor CSC - IBM.</span>
+          </li>
+        </ol>
+      </div>
+      </q-timeline-entry>
+    </q-timeline>
+
+    <q-timeline>
+      <q-separator />
+      <q-timeline-entry heading>
+        <p class="text-h4">
+          Education
+        </p>
+      </q-timeline-entry>
+      <q-timeline-entry
+        subtitle="Jan, 2015 - Dez, 2017 . 3 years"
+        side="left"
+        color="blue-9"
+      >
+        <div>
+          <ol style="list-style-type: none;">
+            <li>
+              <span class="text-weight-bold">FATEC - SP</span>
+            </li>
+            <li>
+              <span class="text-weight-light">Information Technology for Business</span>
+            </li>
+          </ol>
+        </div>
+      </q-timeline-entry>
+    </q-timeline>
+
+    <q-timeline>
+      <q-timeline-entry
+        subtitle="Jan, 2013 - Dez, 2014 . 2 years"
+        side="left"
+        color="blue-9"
+      >
+        <div>
+          <ol style="list-style-type: none;">
+            <li>
+              <span class="text-weight-bold">SENAC</span>
+            </li>
+            <li>
+              <span class="text-weight-light">informatic technician</span>
+            </li>
+          </ol>
+        </div>
+      </q-timeline-entry>
+    </q-timeline>
+
+    <q-timeline>
+      <q-separator />
+      <q-timeline-entry heading>
+        <p class="text-h4">
+          Certifications
+        </p>
+      </q-timeline-entry>
+      <q-timeline-entry
+        subtitle="Feb, 2021"
+        side="left"
+        color="blue-9"
+      >
+        <div>
+          <ol style="list-style-type: none;">
+            <li>
+              <span class="text-weight-bold">IBM Robotic Process Automation - Basic (Portuguese)</span>
+            </li>
+            <li>
+              <a target="_blank" href="https://www.youracclaim.com/badges/9d63a19b-270a-40f1-b5e3-1b32a4e16d8d?source=linked_in_profile">
+                View my verified achievement from IBM.
+              </a>
+            </li>
+          </ol>
+        </div>
+      </q-timeline-entry>
+    </q-timeline>
+
+      <q-separator />
+      <div class="q-my-md">
+        <p class="text-h4">
+          Contact Information
+        </p>
+      </div>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="green" text-color="white" icon="phone" />
+        </q-item-section>
+        <q-item-section>
+           <a
+            style="text-decoration: none;"
+            href="tel:5544991231382">
+            55+ (44) 99123-1382
+          </a>
+        </q-item-section>
+      </q-item>
+      <q-item class="q-my-md" clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="red" text-color="white" icon="mail" />
+        </q-item-section>
+        <q-item-section>
+          <a
+            style="text-decoration: none;"
+            href="mailto:matheusbloisev@hotmail.com">
+            matheusbloisev@hotmail.com
+          </a>
+        </q-item-section>
+      </q-item>
+      <q-item class="q-my-md" clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="blue-9" text-color="white" icon="share" />
+        </q-item-section>
+        <q-item-section>
+          <a
+            target="_blank"
+            style="text-decoration: none;"
+            href="https://www.linkedin.com/in/matheus-b-9609b885/">Linkedin profile
+          </a>
+        </q-item-section>
+      </q-item>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+    }
+  }
+}
+</script>
